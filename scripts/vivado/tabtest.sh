88db63fe47c786ecc4113e8164a95f01392917a4
stride <stride_anderson@qq.com>
2023-02-22 18:02:26 +0800

    improve tabletest.sh

--- scripts/vivado/tabtest.sh
+++ scripts/vivado/tabtest.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+_str_date="[$(date --rfc-3339="ns")]"
+_str_white_red="\033[41;37m"
+_str_red="\033[31m"
+_str_green="\033[32m"
+_str_nomal="\033[0m"
+_str_alarm="\a"
+
+echo_red_white_style() {
+	echo -e "${_str_white_red}${1}${_str_nomal}${_str_alarm}"
+}
+echo_green_style() {
+	echo -e "${_str_green}${1}${_str_nomal}"
+}
+echo_red_style() {
+	echo -e "${_str_red}${1}${_str_nomal}"
+}
+time_stamp(){
+	echo -n "${_str_date}"
+}
+
 set -e
 read _ ip dev grade _ < <( echo $* | tr '_/' ' '; )
 
@@ -13,6 +33,7 @@ step=16
 
 synth_case() {
 	if [ -f test_${1}.txt ]; then
+		time_stamp
 		echo "Reusing cached tab_${ip}_${dev}_${grade}/test_${1}."
 		return
 	fi
@@ -47,6 +68,7 @@ synth_case() {
 		create_clock -period ${speed%?}.${speed#?} [get_ports clk]
 	EOT
 
+	time_stamp 
 	echo "Running tab_${ip}_${dev}_${grade}/test_${1}.."
 	if ! $VIVADO -nojournal -log test_${1}.log -mode batch -source test_${1}.tcl > /dev/null 2>&1; then
 		cat test_${1}.log
@@ -62,19 +84,21 @@ countdown=2
 while [ $countdown -gt 0 ]; do
 	synth_case $speed
 
-	if grep -q '^Slack.*(VIOLATED)' test_${speed}.txt; then
-		echo "        tab_${ip}_${dev}_${grade}/test_${speed} VIOLATED"
+	time_stamp 
+	if grep '^Slack.*(VIOLATED)' test_${speed}.txt; then
+		echo_red_style "        tab_${ip}_${dev}_${grade}/test_${speed} VIOLATED"
 		step=$((step / 2))
 		speed=$((speed + step))
 		got_violated=true
-	elif grep -q '^Slack.*(MET)' test_${speed}.txt; then
-		echo "        tab_${ip}_${dev}_${grade}/test_${speed} MET"
+	elif grep '^Slack.*(MET)' test_${speed}.txt; then
+		echo_green_style "        tab_${ip}_${dev}_${grade}/test_${speed} MET"
 		[ $speed -lt $best_speed ] && best_speed=$speed
 		step=$((step / 2))
 		speed=$((speed - step))
 		got_met=true
 	else
-		echo "ERROR: No slack line found in $PWD/test_${speed}.txt!"
+		echo_red_white_style "ERROR: No slack line found in $PWD/test_${speed}.txt!"
+		echo_red_white_style "$(grep 'Slack' test_${speed}.txt)"
 		exit 1
 	fi
 
@@ -86,16 +110,19 @@ while [ $countdown -gt 0 ]; do
 done
 
 if ! $got_violated; then
-	echo "ERROR: No timing violated in $PWD!"
+	time_stamp 
+	echo_red_white_style "ERROR: No timing violated in $PWD!"
 	exit 1
 fi
 
 if ! $got_met; then
-	echo "ERROR: No timing met in $PWD!"
+	time_stamp 
+	echo_red_white_style "ERROR: No timing met in $PWD!"
 	exit 1
 fi
 
-
+time_stamp 
+echo ""
 echo "-----------------------"
 echo "Best speed for tab_${ip}_${dev}_${grade}: $best_speed"
 echo "-----------------------"
